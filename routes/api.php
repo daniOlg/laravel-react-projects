adb7d8eb3be00414cf382200bcafd59eb511ebbd
Daniel Olguin <dani.olg@hotmail.com>
2024-09-07 16:36:32 -0400

    feat: add auth api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Unauthenticated routes
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function () {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
+
+// Authenticated routes
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'auth'
+], function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::post('/me', [AuthController::class, 'me'])->name('me');
+});
+
