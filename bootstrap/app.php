338291e87245d5de78948b409b5164b607821c5d
Daniel Olguin <dani.olg@hotmail.com>
2024-09-07 19:53:48 -0400

    feat: modify auth exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,24 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(fn(AuthenticationException $e) => response()->json([
+            'success' => false,
+            'message' => 'Please login to access this resource',
+            'error' => $e->getMessage()
+        ], 401));
     })->create();
