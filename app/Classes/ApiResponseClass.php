76b0b895843ff6678b1b0bc32946eb568cbfdb15
Daniel Olguin <dani.olg@hotmail.com>
2024-09-07 21:29:59 -0400

    feat: handle not found exception

--- app/Classes/ApiResponseClass.php
+++ app/Classes/ApiResponseClass.php
@@ -2,6 +2,7 @@
 
 namespace App\Classes;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
@@ -11,17 +12,22 @@ class ApiResponseClass
 {
     public static function rollback($e, $message = "Something went wrong! Process not completed", $code = 500): JsonResponse
     {
+        Log::error($e);
         DB::rollBack();
+
+        if ($e instanceof ModelNotFoundException) {
+            self::throw($e, 'Resource not found', 404);
+        }
+
         self::throw($e, $message, $code);
     }
 
     public static function throw($e, $message = "Something went wrong! Process not completed", $code = 500)
     {
-        Log::info($e);
         throw new HttpResponseException(response()->json([
             'success' => false,
             'message' => $message,
-            'error' => $e->getMessage()
+            'error' => $e->getMessage() ?? 'Error',
         ], $code));
     }
 
