e94bb2746d2166997dcbd03ff9ea1936b920297a
Daniel Olguin <dani.olg@hotmail.com>
2024-09-07 21:52:36 -0400

    fix: use uuids

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,8 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    use HasFactory;
-    use HasUuids;
+    use HasFactory, HasUuids;
 
     protected $fillable = [
         'name',
